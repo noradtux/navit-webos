0c5218a06aa1933b56138510619c164f8bed3d92
Malte Schröder <maltesch@gmx.de>
2011-06-13 12:54:34 +0200

    apply-development-changes

--- navit/navit/speech/speech_dispatcher/speech_speech_dispatcher.c
+++ navit/navit/speech/speech_dispatcher/speech_speech_dispatcher.c
@@ -27,6 +27,7 @@
 #include <glib.h>
 #include <stdarg.h>
 #include "config.h"
+#include "item.h"
 #include <libspeechd.h>
 #include "plugin.h"
 #include "speech.h"
@@ -59,6 +60,8 @@ static struct speech_methods speechd_meth = {
 static struct speech_priv *
 speechd_new(struct speech_methods *meth, struct attr **attrs, struct attr *attr) {
 	struct speech_priv *this;
+	struct attr *volume, *language;
+	char *lang_str;
 	SPDConnection *conn;
 
 	conn = spd_open("navit","main",NULL,SPD_MODE_SINGLE);
@@ -68,7 +71,33 @@ speechd_new(struct speech_methods *meth, struct attr **attrs, struct attr *attr)
 	if (this) {
 		this->conn=conn;
 		*meth=speechd_meth;
+		volume = attr_search(attrs, NULL, attr_data);
+		language = attr_search(attrs, NULL, attr_language);
+		if (language) {
+			lang_str=g_strdup(language->u.str);
+		} else {
+			char *lang_env=getenv("LANG");
+
+			if (lang_env) {
+				char *country,*lang,*lang_full;
+				lang_full=g_strdup(lang_env);
+				strtolower(lang_full,lang_env);
+				lang=g_strdup(lang_full);
+				country=strchr(lang_full,'_');
+				if (country) {
+					lang[country-lang_full]='\0';
+					*country++='-';
+				}
+				lang_str=g_strdup(lang);
+				g_free(lang_full);
+				g_free(lang);
+			}
+		}
 		spd_set_punctuation(conn, SPD_PUNCT_NONE);
+		spd_set_volume(conn, volume->u.num);
+		spd_set_language(conn, lang_str);
+		if (lang_str)
+			g_free(lang_str);
 	}
 	return this;
 }
